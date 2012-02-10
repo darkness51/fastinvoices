4f212d10e00cd956e961391e3093fa4ec76594a6
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-02-09 22:32:44 -0600

    Added django_countries to installed_apps

--- settings.py
+++ settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'main',
+    'django_countries',
 )
 
 # A sample logging configuration. The only tangible logging
