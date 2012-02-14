f8378e8fd63365fa004aa0072439a611ee8a5862
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-02-14 00:00:50 -0600

    Starting to write views

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from models import *
+from datetime import datetime
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from pprint import pprint
+
+import logging
+
+logger = logging.getLogger(__name__)
