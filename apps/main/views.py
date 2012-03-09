6cb057ceab47ef81fbda4efdb876255e751cbebf
Carlos Aguilar <caguilar@dwdandsolutions.com>
2012-03-09 01:19:53 -0600

    Changed the view using class based views and created classes for products views

--- apps/main/views.py
+++ apps/main/views.py
@@ -5,19 +5,23 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.contrib.auth import logout
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView, ListView, CreateView
 from pprint import pprint
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-def index(request):
-    '''
-    Main view for the application
-    '''
+class IndexView(TemplateView):
+    template_name = "index.html"
     
-    return render_to_response("index.html", context_instance=RequestContext(request))
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 10
     
+class ProductAddView(CreateView):
+    model = Product
+        
 def logout_page(request):
     '''
     Finish the user session
